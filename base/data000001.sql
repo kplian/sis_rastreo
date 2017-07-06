0dc54bd252d33fa0a0bf2926a2b56364964c966e
Rodrigo Chumacero <rodrychm@gmail.com>
2017-07-06 10:21:29 -0400

    correccion menu consultas

--- base/data000001.sql
+++ base/data000001.sql
@@ -12,7 +12,7 @@ select pxp.f_insert_tgui ('Condutores', 'Registro Conductores', 'RAS.2', 'si', 4
 select pxp.f_insert_tgui ('Vehículos', 'Registro de Vehículos', 'RAS.3', 'si', 5, 'sis_rastreo/vista/equipo/Equipo.php', 2, '', 'Equipo', 'RAS');
 select pxp.f_insert_tgui ('Ubicación de Vehículos', 'Búsqueda de vehículos por diferentes criterios', 'RAS.4', 'si', 1, '', 2, '', '', 'RAS');
 select pxp.f_insert_tgui ('Posiciones capturadas', 'Posiciones capturadas', 'RAS.5', 'si', 2, 'sis_rastreo/vista/posicion/Posicion.php', 3, '', 'Posicion', 'RAS');
-select pxp.f_insert_tgui ('Consultas', 'Consultas', 'RAS.6', 'si', 4, 'sis_rastreo/vista/posicion/Consultas.php', 3, '', 'Consultas', 'RAS');
+select pxp.f_insert_tgui ('Consultas', 'Consultas', 'RAS.6', 'si', 4, 'sis_rastreo/vista/positions/Consultas.php', 3, '', 'Consultas', 'RAS');
 select pxp.f_insert_tgui ('Reportes', 'Búsqueda de vehículos por diferentes criterios', 'RAS.7', 'si', 1, '', 2, '', '', 'RAS');
 
 select pxp.f_insert_tgui ('Marcas', 'Marcas y modelos', 'RAS.8', 'si', 2, 'sis_rastreo/vista/marca/Marca.php', 3, '', 'Marca', 'RAS');
@@ -35,4 +35,7 @@ select pxp.f_insert_testructura_gui ('RAS.9', 'RAS');
 /***********************************I-DAT-RCM-RAS-0-06/07/2017****************************************/
 select pxp.f_add_catalog('RAS','tequipo__traccion','2WD','2WD','');
 select pxp.f_add_catalog('RAS','tequipo__traccion','4WD','4WD','');
+
+select pxp.f_add_catalog('RAS','tequipo__cabina','Simple','simple','');
+select pxp.f_add_catalog('RAS','tequipo__cabina','Doble','doble','');
 /***********************************I-DAT-RCM-RAS-0-06/07/2017****************************************/
