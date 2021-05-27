38d179651bfba809874849cf14343227afa67993
jjimenez <juan.jimenez@endetransmision.bo>
2021-05-27 13:19:54 -0400

    #RAS-8 Cambio de nombre de columna ceco

--- reportes/Rdetalle_solicitud_vehiculo.php
+++ reportes/Rdetalle_solicitud_vehiculo.php
@@ -50,7 +50,7 @@ header("content-type: text/javascript; charset=UTF-8");
                     {
                         config:{
                             name: 'ceco',
-                            fieldLabel: 'Depto',
+                            fieldLabel: 'Ceco',
                             allowBlank: true,
                             anchor: '80%',
                             gwidth: 250,
